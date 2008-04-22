62e7e6bb5f17beeb1ab89405242d058943b54734
jason.schramm <jason.schramm@7dbfc49c-5a49-0410-b78d-15e85b6e0296>
2008-04-22 15:14:30 +0000

    Removed duplicate import for ArrayList class.

--- trunk/OOPAP/OOPAP/src/edu/monmouth/se/oopap/sourceanalyzer/NumberOfChildrenSourceAnalyzer.java
+++ trunk/OOPAP/OOPAP/src/edu/monmouth/se/oopap/sourceanalyzer/NumberOfChildrenSourceAnalyzer.java
@@ -3,7 +3,6 @@ package edu.monmouth.se.oopap.sourceanalyzer;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.ArrayList;
 import java.util.Map;
 import java.util.Set;
 
