adf2925fa84936efdf519cdfad4891457e020012
atasso <atasso@7dbfc49c-5a49-0410-b78d-15e85b6e0296>
2008-04-30 02:53:13 +0000

    Implemented ability to save to user specific directory

--- trunk/OOPAP/OOPAP/src/edu/monmouth/se/oopap/OOPAPGUI.java
+++ trunk/OOPAP/OOPAP/src/edu/monmouth/se/oopap/OOPAPGUI.java
@@ -22,12 +22,14 @@ import javax.swing.JLabel;
 public class OOPAPGUI extends JFrame
 {
 
-  private JTextField textField;
+  private JTextField inputPathTextField;
+  private JTextField outputPathTextField;  
   private JTextField studentNameTextField;
   private JTextField projectNameTextField;
   private OOPAPController controller;
   private JFileChooser fileChooser;
-  private String workingPath;
+  private String inputPath;
+  private String outputPath;
   private static final String sourceExtension = ".java";
   
   /**
@@ -45,40 +47,48 @@ public class OOPAPGUI extends JFrame
 
     this.controller = theController;
     this.fileChooser = new JFileChooser();
-    this.fileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-    this.workingPath = System.getProperty("user.dir");
+    this.inputPath = System.getProperty("user.dir");
+    this.outputPath = System.getProperty("user.dir");
+    this.console = new OOPAPConsole();
     this.studentNameTextField = new JTextField();
     this.projectNameTextField = new JTextField();
-    
+    this.outputPathTextField = new JTextField();
+    JLabel inputLabel = new JLabel("Input:");
+    JLabel outputLabel = new JLabel("Output:");    
     JLabel studentNameLabel = new JLabel("Student Name:");
     JLabel projectNameLabel = new JLabel("Project Name:");
 
     getContentPane().setLayout(null);
-    setBounds(100, 100, 425, 145);
+    setBounds(100, 100, 510, 180);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     super.setTitle("OOPAP");
 
     //Bound the labels for the student an project name and add
     //them to the interface
-    studentNameLabel.setBounds(10, 50, 100, 20);
+    inputLabel.setBounds(10, 10, 50, 20);
+    getContentPane().add(inputLabel);
+    outputLabel.setBounds(10, 40, 50, 20);
+    getContentPane().add(outputLabel);
+    getContentPane().add(inputLabel);
+    studentNameLabel.setBounds(10, 80, 100, 20);
     getContentPane().add(studentNameLabel);
-    projectNameLabel.setBounds(10, 85, 100, 20);
+    projectNameLabel.setBounds(10, 115, 100, 20);
     getContentPane().add(projectNameLabel);
     
     //set the bounds for the student and project name text fields
-    this.studentNameTextField.setBounds(110, 50, 175, 26);
+    this.studentNameTextField.setBounds(110, 80, 175, 26);
     getContentPane().add(this.studentNameTextField);
-    this.projectNameTextField.setBounds(110, 85, 175, 26);
+    this.projectNameTextField.setBounds(110, 115, 175, 26);
     getContentPane().add(this.projectNameTextField);
 
     
-    textField = new JTextField();
-    textField.setBounds(10, 10, 359, 26);
-    getContentPane().add(textField);
-    this.textField.setText(workingPath);
+    inputPathTextField = new JTextField();
+    inputPathTextField.setBounds(65, 10, 400, 25);
+    getContentPane().add(inputPathTextField);
+    this.inputPathTextField.setText(inputPath);
 
-    final JButton chooseFileButton = new JButton();
-    chooseFileButton.addActionListener(new ActionListener()
+    final JButton chooseInputPathButton = new JButton();
+    chooseInputPathButton.addActionListener(new ActionListener()
     {
 
       public void actionPerformed(final ActionEvent e)
@@ -90,10 +100,30 @@ public class OOPAPGUI extends JFrame
 
     });
 
-    chooseFileButton.setText("...");
-    chooseFileButton.setBounds(375, 10, 34, 26);
-    getContentPane().add(chooseFileButton);
+    chooseInputPathButton.setText("...");
+    chooseInputPathButton.setBounds(470, 10, 34, 26);
+    getContentPane().add(chooseInputPathButton);
+    
+    outputPathTextField.setBounds(65, 40, 400, 25);
+    getContentPane().add(outputPathTextField);
+    this.outputPathTextField.setText(outputPath);
+    
+    final JButton chooseOutputPathButton = new JButton();
+    chooseOutputPathButton.addActionListener(new ActionListener()
+    {
 
+      public void actionPerformed(final ActionEvent e)
+      {
+
+        selectOutputPath();
+
+      }
+
+    });
+    chooseOutputPathButton.setText("...");
+    chooseOutputPathButton.setBounds(470, 40, 34, 26);
+    getContentPane().add(chooseOutputPathButton);    
+    
     final JButton runAnalysisButton = new JButton();
     runAnalysisButton.addActionListener(new ActionListener()
     {
@@ -103,16 +133,17 @@ public class OOPAPGUI extends JFrame
 
         try
         {
+
+          
           controller.clearFullConsoleReport();
-          workingPath = textField.getText();
-          controller.runAnalysis(workingPath, sourceExtension,
+        	
+          inputPath = inputPathTextField.getText();
+          controller.runAnalysis(inputPath, sourceExtension,
+                                 outputPath,
                                  studentNameTextField.getText(),
                                  projectNameTextField.getText());
           
           List<String> fullConsoleReport = controller.getFullConsoleReport();
-          
-          console = new OOPAPConsole();
-
           console.displayConsoleReport(fullConsoleReport);
           console.setVisible(true);
           
@@ -128,27 +159,48 @@ public class OOPAPGUI extends JFrame
 
     });
     runAnalysisButton.setText("Run Analysis");
-    runAnalysisButton.setBounds(298, 60, 120, 26);
-    getContentPane().add(runAnalysisButton);
-    //
-
+    runAnalysisButton.setBounds(335, 95, 150, 26);
+    getContentPane().add(runAnalysisButton);    
+    
     this.setVisible(true);
   }
 
   protected void selectFile()
   {
+   
+    this.fileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
 
-    fileChooser.setCurrentDirectory(new File(this.workingPath));
+    fileChooser.setCurrentDirectory(new File(this.inputPath));
     int returnVal = fileChooser.showOpenDialog(this);
 
     if (returnVal == JFileChooser.APPROVE_OPTION)
     {
 
       File file = fileChooser.getSelectedFile();
-      workingPath = file.toString();
-      textField.setText(workingPath);
+      inputPath = file.toString();
+      inputPathTextField.setText(inputPath);
 
     }
 
   }
+  
+  protected void selectOutputPath()
+  {
+
+    this.fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+    fileChooser.setCurrentDirectory(new File(this.outputPath));
+    int returnVal = fileChooser.showOpenDialog(this);
+
+    if (returnVal == JFileChooser.APPROVE_OPTION)
+    {
+
+      File file = fileChooser.getSelectedFile();
+      this.outputPath = file.toString();
+      this.outputPathTextField.setText(outputPath);
+
+    }
+
+  }
+  
 }
